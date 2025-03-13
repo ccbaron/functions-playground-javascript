e85d058148a2bd919e35dd2f79704c336c941e4c
ccbaron <christian7baron@gmail.com>
2025-03-13 21:45:56 +0100

    ejercicio 4

--- ex3b.js
+++ ex3b.js
@@ -10,12 +10,15 @@ function isPositive(num) {
  * 1. ¿Que nombre tiene esta función?: isPositive
  * 2. ¿Que deberíamos escribir para ejecutar esta función?: isPositive(5) 
  * 3. ¿Qué tipo de datos devuelve esta función? ¿Un string, un number o un boolean?: Un boolean
- * 4. El código de abajo usa la función isPositve. ¿La usa adecuadamente? ¿Por qué? ¿Qué debería cambiar?: No, porque el número que se le pasa es 4, que es positivo. Debería cambiar el número que se le pasa a un número negativo.
+ * 4. El código de abajo usa la función isPositve. ¿La usa adecuadamente? ¿Por qué? ¿Qué debería cambiar?: No, porque el número que se le pasa es 4, que es positivo. Debería cambiar los mensajes de los console.log.
  */
 
 let x = 4;
 if (isPositive(x)) {
-  console.log("El número es negativo");
-} else {
   console.log("El número es positivo");
+} else {
+  console.log("El número es negativo");
 }
+
+//let resultado = isPositive(1);
+//console.log(resultado)
--- ex4.js
+++ ex4.js
@@ -4,7 +4,10 @@
 
 function rollDice() {
     // COMPLETAR
-    return Math.floor(Math.random() * 6) + 1; ; // Devuelve un número entre 1 y 6 porque Math.random() devuelve un número entre 0 y 1, y al multiplicarlo por 6, obtenemos un número entre 0 y 6. Al sumarle 1, obtenemos un número entre 1 y 6.
+    let result = Math.floor(Math.random() * 6) + 1; 
+    return result;
+    
+    // Devuelve un número entre 1 y 6 porque Math.random() devuelve un número entre 0 y 1, y al multiplicarlo por 6, obtenemos un número entre 0 y 6. Al sumarle 1, obtenemos un número entre 1 y 6.
 }
 
 console.log(rollDice()); // Un número entre 1 y 6
